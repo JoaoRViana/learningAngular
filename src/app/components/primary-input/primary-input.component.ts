e68efbd48fae3fef22cda9d461311daa3e6c3af9
JoaoRViana <joaorviana98@gmail.com>
2024-09-10 11:01:51 -0300

    criando input

---
+++ src/app/components/primary-input/primary-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+type inputTypes = 'text'|'email'|'password'
+
+@Component({
+  selector: 'app-primary-input',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './primary-input.component.html',
+  styleUrl: './primary-input.component.scss'
+})
+export class PrimaryInputComponent {
+  @Input() type:inputTypes = 'text';
+  @Input() formName:string = '';
+  @Input() placeholder:string = '';
+}
